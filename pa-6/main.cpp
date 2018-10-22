213c32b6fe69f9826306cb0f25a29a18bd5c4987
akeypj <pjakey@gmail.com>
2018-10-21 22:58:31 -0400

    pushing final changes to pa-6

--- pa-6/main.cpp
+++ pa-6/main.cpp
@@ -31,7 +31,6 @@ class invalidEntry {
 
 // prototype the functions
 void runProgram();
-std::string checkInputStr();
 int checkInputInt(std::string);
 
 // main
@@ -42,93 +41,60 @@ int main() {
 
 // get user input
 void runProgram() {
-    unorderedLinkedList<int> linkedList, list2;
+    queueType<int> queue1, queue2;
 
     int userInput = 0;
     try {
         do {
-            std::cout << std::endl << "Welcome to PA-5 - Linked Lists"
+            std::cout << std::endl << "Welcome to PA-6 - Queue As Array"
                         << std::endl;
-            std::cout << "   1 - Add a new item to the list" << std::endl;
-            std::cout << "   2 - Delete the first occurrence of a given item "
-                        << "from the list" << std::endl;
-            std::cout << "   3 - Delete all occurrences of a given item from "
-                        << "the list" << std::endl;
-            std::cout << "   4 - Delete the smallest item from the list"
-                        << std::endl;
-            std::cout << "   5 - Print the kth item in the list" << std::endl;
-            std::cout << "   6 - Delete the kth item from the list"
-                        << std::endl;
-            std::cout << "   7 - Print the list in reverse" << std::endl;
-            std::cout << "   8 - Print the list" << std::endl;
-            std::cout << "   9 - Quit" << std::endl;
+            std::cout << "   1 - enqueue" << std::endl;
+            std::cout << "   2 - dequeue" << std::endl;
+            std::cout << "   3 - copy queue" << std::endl;
+            std::cout << "   4 - print the original queue" << std::endl;
+            std::cout << "   5 - print the copy queue" << std::endl;
+            std::cout << "   6 - debug queue" << std::endl;
+            std::cout << "   7 - quit" << std::endl;
             userInput = checkInputInt("Make your selection");
 
             if (userInput == 1) {
-                // Add a new item to the list
+                // enqueue
                 int info;
                 info = checkInputInt("Enter your new item");
-                linkedList.insertFirst(info);
+                queue1.enqueue(info);
                 std::cout << info << " has been inserted into the beginning "
                             << "of your list." << std::endl;
             } else if (userInput == 2) {
-                // Delete the first occurrence of an item
-                // AND stop traversing the list!
-                int info;
-                std::cout << "You've chosen to remove the first occurrence "
-                            << "of an item." << std::endl;
-                info = checkInputInt("Enter the value to be removed");
-                linkedList.deleteItem(info);
+                // dequeue
+                std::cout << "You've chosen to dequeue" << std::endl;
+                queue1.dequeue();
             } else if (userInput == 3) {
-                // Delete all occurrences of an item
-                int info;
-                std::cout << "You've chosen to remove all occurrences "
-                            << "of an item." << std::endl;
-                info = checkInputInt("Enter the value to be removed");
-                linkedList.deleteAll(info);
+                // copy queue
+                std::cout << "Copying queue..." << std::endl;
+                queue2 = queue1;
+                std::cout << "Done!" << std::endl;
             } else if (userInput == 4) {
-                // Delete the smallest item from the list
-                std::cout << "You've chosen to remove the smallest item"
-                            << " in your list." << std::endl;
-                linkedList.deleteSmallest();
+                // print the original queue
+                std::cout << "Your primary queue contains the "
+                            << "following elements" << std::endl;
+                queue1.printQueue();
             } else if (userInput == 5) {
-                // Print the kth item in the list
-                int info;
-                std::cout << "You've chosen to print the kth item in the list"
-                            << std::endl;
-                std::string msg = "Enter the position of the element you "
-                            "would like to print\n"
-                            "(the \"position\" starts at 1)";
-                info = checkInputInt(msg);
-                linkedList.print(info);
+                // print the copy queue
+                std::cout << "Printing the copied queue:" << std::endl;
+                queue2.printQueue();
             } else if (userInput == 6) {
-                // Delete the kth item in the list
-                int info;
-                std::cout << "You've chosen to delete the kth item in the list"
-                            << std::endl;
-                std::string msg = "Enter the position of the element you "
-                            "would like to delete\n"
-                            "(the \"position\" starts at 1)";
-                info = checkInputInt(msg);
-                linkedList.deleteNode(info);
-            } else if (userInput == 7) {
-                // Print the list in reverse
-                std::cout << "Recursively:" << std::endl;
-                linkedList.recursiveReversePrint();
-                // Does not work, unsure how to instantiate an object
-                // while that object is being defined :)
-                std::cout << "Non-Recursively (poorly implemented):"
-                        << std::endl;
-                linkedList.reversePrint();
-            } else if (userInput == 8) {
-                // Print the list
-                linkedList.print();
-            } else if (userInput != 9) {
+                // debug queue
+                std::cout << std::endl << "*********************" << std::endl;
+                std::cout << "queue1 debug info:" << std::endl;
+                queue1.debugQueue();
+                std::cout << std::endl << "*********************" << std::endl;
+                std::cout << "queue2 debug info:" << std::endl;
+                queue2.debugQueue();
+            } else if (userInput != 7) {
                 std::cout << "Invalid entry. Select an option from the list:"
                         << std::endl;
             }
-        }
-        while (userInput != 9);
+        } while (userInput != 7);
         std::cout << "All done!  Exiting." << std::endl;
     } catch (invalidEntry& e) {
         std::cout << e.what() << std::endl;
@@ -146,7 +112,7 @@ int checkInputInt(std::string strMsg) {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             std::cout << "The value entered was either not an integer or\n"
-                    << "outside of the range of an int (+/-2147483648)."
+                    << "outside of the range of an acceptable integer."
                     << std::endl;
         } else {
             return num;
--- pa-6/queueAsArray.h
+++ pa-6/queueAsArray.h
@@ -9,190 +9,229 @@
 
 #include "queueADT.h"
 
-using namespace std;
-
 template <class Type>
-class queueType: public queueADT<Type>
-{
-public:
+class queueType: public queueADT<Type> {
+ public:
     const queueType<Type>& operator=(const queueType<Type>&);
-      //Overload the assignment operator.
+      // Overload the assignment operator.
 
     bool isEmptyQueue() const;
-      //Function to determine whether the queue is empty.
-      //Postcondition: Returns true if the queue is empty,
-      //               otherwise returns false.
+      // Function to determine whether the queue is empty.
+      // Postcondition: Returns true if the queue is empty,
+      //                otherwise returns false.
 
     bool isFullQueue() const;
-      //Function to determine whether the queue is full.
-      //Postcondition: Returns true if the queue is full,
-      //               otherwise returns false.
+      // Function to determine whether the queue is full.
+      // Postcondition: Returns true if the queue is full,
+      //                otherwise returns false.
 
     void initializeQueue();
-      //Function to initialize the queue to an empty state.
-      //Postcondition: The queue is empty.
+      // Function to initialize the queue to an empty state.
+      // Postcondition: The queue is empty.
 
     Type front() const;
-      //Function to return the first element of the queue.
-      //Precondition: The queue exists and is not empty.
-      //Postcondition: If the queue is empty, the program
-      //               terminates; otherwise, the first
-      //               element of the queue is returned.
+      // Function to return the first element of the queue.
+      // Precondition: The queue exists and is not empty.
+      // Postcondition: If the queue is empty, the program
+      //                terminates; otherwise, the first
+      //                element of the queue is returned.
+
     Type back() const;
-      //Function to return the last element of the queue.
-      //Precondition: The queue exists and is not empty.
-      //Postcondition: If the queue is empty, the program
-      //               terminates; otherwise, the last
-      //               element of the queue is returned.
+      // Function to return the last element of the queue.
+      // Precondition: The queue exists and is not empty.
+      // Postcondition: If the queue is empty, the program
+      //                terminates; otherwise, the last
+      //                element of the queue is returned.
 
-    void addQueue(const Type& queueElement);
-      //Function to add queueElement to the queue.
-      //Precondition: The queue exists and is not full.
-      //Postcondition: The queue is changed and queueElement
-      //               is added to the queue.
+    void enqueue(const Type& queueElement);
+      // Function to add queueElement to the queue.
+      // Precondition: The queue exists and is not full.
+      // Postcondition: The queue is changed and queueElement
+      //                is added to the queue.
 
-    void deleteQueue();
-      //Function to remove the first element of the queue.
-      //Precondition: The queue exists and is not empty.
-      //Postcondition: The queue is changed and the first
-      //               element is removed from the queue.
+    void dequeue();
+      // Function to remove the first element of the queue.
+      // Precondition: The queue exists and is not empty.
+      // Postcondition: The queue is changed and the first
+      //                element is removed from the queue.
+
+    void debugQueue() const;
+      // Function to show properties of the queue for debugging purposes
+
+    void printQueue() const;
+      // Function to print contents of queueAsArray
 
     queueType(int queueSize = 100);
-      //Constructor
+      // Constructor
 
     queueType(const queueType<Type>& otherQueue);
-      //Copy constructor
+      // Copy constructor
 
     ~queueType();
-      //Destructor
+      // Destructor
 
-private:
-    int maxQueueSize; //variable to store the maximum queue size
-    int count;        //variable to store the number of
-                      //elements in the queue
-    int queueFront;   //variable to point to the first
-                      //element of the queue
-    int queueRear;    //variable to point to the last
-                      //element of the queue
-    Type *list;       //pointer to the array that holds
-                      //the queue elements
+ private:
+    int maxQueueSize;  // variable to store the maximum queue size
+    int count;         // variable to store the number of
+                       // elements in the queue
+    int queueFront;    // variable to point to the first
+                       // element of the queue
+    int queueRear;     // variable to point to the last
+                       // element of the queue
+    Type *q;           // pointer to the array that holds
+                       // the queue elements
 };
 
 template <class Type>
-bool queueType<Type>::isEmptyQueue() const
-{
+bool queueType<Type>::isEmptyQueue() const {
     return (count == 0);
-} //end isEmptyQueue
+}  // end isEmptyQueue
 
 template <class Type>
-bool queueType<Type>::isFullQueue() const
-{
+bool queueType<Type>::isFullQueue() const {
     return (count == maxQueueSize);
-} //end isFullQueue
+}  // end isFullQueue
 
 template <class Type>
-void queueType<Type>::initializeQueue()
-{
+void queueType<Type>::initializeQueue() {
     queueFront = 0;
     queueRear = maxQueueSize - 1;
     count = 0;
-} //end initializeQueue
+}  // end initializeQueue
 
+// Pointer to the front of the queue
 template <class Type>
-Type queueType<Type>::front() const
-{
+Type queueType<Type>::front() const {
     assert(!isEmptyQueue());
-    return list[queueFront];
-} //end front
+    return q[queueFront];
+}  // end front
 
+// Pointer to the rear of the queue
 template <class Type>
-Type queueType<Type>::back() const
-{
+Type queueType<Type>::back() const {
     assert(!isEmptyQueue());
-    return list[queueRear];
-} //end back
+    return q[queueRear];
+}  // end back
 
+// Add to the queue
 template <class Type>
-void queueType<Type>::addQueue(const Type& newElement)
-{
-    if (!isFullQueue())
-    {
-        queueRear = (queueRear + 1) % maxQueueSize; //use mod
-                            //operator to advance queueRear
-                            //because the array is circular
+void queueType<Type>::enqueue(const Type& newElement) {
+    if (!isFullQueue()) {
+        queueRear = (queueRear + 1) % maxQueueSize;
+                            // use mod operator to advance queueRear
+                            // because the array is circular
         count++;
-        list[queueRear] = newElement;
+        q[queueRear] = newElement;
+    } else {
+        std::cout << "Cannot add to a full queue." << std::endl;
     }
-    else
-        cout << "Cannot add to a full queue." << endl;
-} //end addQueue
+}  // end enqueue
 
+// Remove from queue
 template <class Type>
-void queueType<Type>::deleteQueue()
-{
-    if (!isEmptyQueue())
-    {
+void queueType<Type>::dequeue() {
+    if (!isEmptyQueue()) {
         count--;
-        queueFront = (queueFront + 1) % maxQueueSize; //use the
-                        //mod operator to advance queueFront
-                        //because the array is circular
+        queueFront = (queueFront + 1) % maxQueueSize;  // use the
+                                        // mod operator to advance queueFront
+                                        // because the array is circular
+    } else {
+        std::cout << "Cannot remove from an empty queue." << std::endl;
     }
-    else
-        cout << "Cannot remove from an empty queue." << endl;
-} //end deleteQueue
+}  // end dequeue
 
-    //Constructor
+// Debug queue
 template <class Type>
-queueType<Type>::queueType(int queueSize)
-{
-    if (queueSize <= 0)
-    {
-        cout << "Size of the array to hold the queue must "
-             << "be positive." << endl;
-        cout << "Creating an array of size 100." << endl;
+void queueType<Type>::debugQueue() const {
+    if (count > 0) {
+      std::cout << std::endl;
+      std::cout << "-------Debug Info-------" << std::endl;
+      std::cout << "--Your Queue Contains:--" << std::endl;
+      printQueue();
+      std::cout << "-------Queue Info-------" << std::endl;
+      std::cout << "maxQueueSize = " << maxQueueSize << std::endl;
+      std::cout << "item count = " << count << std::endl;
+      std::cout << "queueFront = " << queueFront << std::endl;
+      std::cout << "Front item = " << q[queueFront] << std::endl;
+      std::cout << "queueRear = " << queueRear << std::endl;
+      std::cout << "Rear item = " << q[queueRear] << std::endl;
+      std::cout << "-----End Debug Info-----" << std::endl << std::endl;
+    } else {
+      std::cout << "Your queue is empty." << std::endl;
+    }
+}  // end debugQueue
+
+// Print queue
+template <class Type>
+void queueType<Type>::printQueue() const {
+    if (count != 0) {
+        for (int i = queueFront; i < queueRear+1; i++)
+            std::cout << q[i] << " ";
+        std::cout << std::endl;
+    } else {
+        std::cout << "<empty>" << std::endl;
+    }
+}  // end printQueue
+
+// Constructor
+template <class Type>
+queueType<Type>::queueType(int queueSize) {
+    if (queueSize <= 0) {
+        std::cout << "Size of the array to hold the queue must "
+             << "be positive." << std::endl;
+        std::cout << "Creating an array of size 100." << std::endl;
 
         maxQueueSize = 100;
+    } else {
+        maxQueueSize = queueSize;   // set maxQueueSize to
+                                    // queueSize
     }
-    else
-        maxQueueSize = queueSize;   //set maxQueueSize to
-                                    //queueSize
-
-    queueFront = 0;                 //initialize queueFront
-    queueRear = maxQueueSize - 1;   //initialize queueRear
+    queueFront = 0;                 // initialize queueFront
+    queueRear = maxQueueSize - 1;   // initialize queueRear
     count = 0;
-    list = new Type[maxQueueSize];  //create the array to
-                                    //hold the queue elements
-} //end constructor
+    q = new Type[maxQueueSize];  // create the array to
+                                    // hold the queue elements
+}  // end constructor
 
-    //Destructor
-template <class Type>
-queueType<Type>::~queueType()
-{
-    delete [] list;
-} //end destructor
+// Destructor
+template <class Type>queueType<Type>::~queueType() {
+    delete [] q;
+}  // end destructor
 
+// Overloading the assignment operator
 template <class Type>
 const queueType<Type>& queueType<Type>::operator=
-	                   (const queueType<Type>& otherQueue)
-{
-    cout << "Write the definition of the function "
-         << "to overload the assignment operator." << endl;
+                       (const queueType<Type>& otherQueue) {
+    // Check that I'm not copying it to itself
     if (this != &otherQueue) {
-
+        // delete existing queue
+        delete [] q;
+        maxQueueSize = otherQueue.maxQueueSize;
+        queueFront = otherQueue.queueFront;
+        queueRear = otherQueue.queueRear;
+        count = otherQueue.count;
+        q = new int[maxQueueSize];
+        for (int i = queueFront; i < queueRear+1; i++)
+            q[i] = otherQueue.q[i];
     }
-} //end assignment operator
+    return *this;
+}  // end assignment operator
 
+// Copy constructor
 template <class Type>
-queueType<Type>::queueType(const queueType<Type>& otherQueue)
-{
-    cout << "Write the definition of the copy constructor."
-         << endl;
+queueType<Type>::queueType(const queueType<Type>& otherQueue) {
     // check to make sure this isn't the same object
     if (this != &otherQueue) {
-        // get max size
-        // make a new array
-        // use for loop to loop through
+        // delete existing queue
+        delete [] q;
+        maxQueueSize = otherQueue.maxQueueSize;
+        queueFront = otherQueue.queueFront;
+        queueRear = otherQueue.queueRear;
+        count = otherQueue.count;
+        q = new int[maxQueueSize];
+        for (int i = queueFront; i < queueRear+1; i++)
+            q[i] = otherQueue.q[i];
     }
-} //end copy constructor
+}  // end copy constructor
 
 #endif
